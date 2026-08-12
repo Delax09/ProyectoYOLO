804df27d87652d687d4fd671bab5c82752cbd09a
Fabian Mejias <fmejias@minutoverde.cl>
2026-08-12 15:45:16 -0400

    Futura implementación de entrenamiento personalizado

---
+++ entrenar.py
@@ -0,0 +1,11 @@
+from ultralytics import YOLO
+
+model = YOLO('yolov8s.pt')
+
+results = model.train(
+    data= 'data.yaml', #Ruta al archivo .yaml
+    epochs = 50, #Epocas para entrenar
+    imgsz = 640, #Resolucion de la imagen
+    batch = 8, #Imagenes procesadas a la vez (bajar si me quedo sin memoria)
+    name= 'casco_yolov8s',
+)
